037354902d69fe84e8d439b3a3c55185047c3339
tommaddoxsoftware <tommaddox1997uk@gmail.com>
2019-01-06 12:19:35 +0000

    Updated Map.JS to use AJAX requests to add markers (FIXED)

--- Website Source/assets/js/map.js
+++ Website Source/assets/js/map.js
@@ -1,5 +1,6 @@
-var gmarkers = [];
-var trees = [];
+var gmarkers    = [];
+var trees       = [];
+var customers   = [];
 var image = 'https://marldonchristmastrees.co.uk/wp-content/themes/Marldon2018/assets/icons/mapmarker.png';
 var map
 
@@ -40,26 +41,16 @@ function UpdateMarkers() {
         url: "http://localhost:9000/GetLocations",
         type: 'POST',
         success: function(result) {
-            result = JSON.parse(result);
+            parsedResult = JSON.parse(result);
 
-            //Store customers in an array
-            var customers = [];
-
-            for(var i=0; i<result.length; i++) {
-                //AddMarker(result[i]);
-                console.log(result);
-
-                //Store in customer array
-                customers.push(result[i].customer, result[i].tree);
+            for(var i=0; i<parsedResult.length; i++) {
+                AddMarker(parsedResult[i].customer, parsedResult[i].tree);
             }
         }
     });
-
-
-
 }
 
-function AddMarkers(custDoc, custTreeDoc) {
+function AddMarker(custDoc, custTreeDoc) {
     //Create marker from doc
     var tempPos = {lat: parseFloat(custTreeDoc.location.lat), lng: parseFloat(custTreeDoc.location.long)};
     var tempMarker = new google.maps.Marker({
@@ -71,79 +62,69 @@ function AddMarkers(custDoc, custTreeDoc) {
     //Store the marker in an array in case we need to clear
     gmarkers.push(tempMarker);
 
+
+    //Store customer in an array
+    customers.push(custDoc);
+    trees.push(custTreeDoc);
+
+    //Store customers and trees in localstorage so we don't have to reaccess database
+    localStorage.setItem("customers", JSON.stringify(customers));
+    localStorage.setItem("trees", JSON.stringify(trees));
+
     var contentString;
+    var customerID = custDoc._id;
 
+    //Try catch, in case data passed to func is wrong
+    try {
+        //Content for infowindow
+        contentString = '<div id="content-user-'+ customerID +'">'+
+        '<div id="siteNotice">'+
+        '</div>'+
+        '<h5 id="firstHeading" class="firstHeading">'+custDoc.first_name +" " + custDoc.last_name+'</h5>'+
+        '<div id="bodyContent">'+
+        '<p class="livemap-head"><strong>'+custDoc.email+'</strong></p>'+
+        '<p class="m-0 livemap-head"><strong>Customer Collection Date:</strong> '+custTreeDoc.shipping_date+'</p>'+
+        '<p class="m-0 livemap-head"><strong>Tree Cutting Date: </strong>' +custTreeDoc.cutting_info.cut_date+'</p>'+
+        '<p class="m-0 livemap-head"><strong>Tree ID: </strong>'+custTreeDoc.tree.id+'</p>'+
+        '<p class="m-0 livemap-head"><strong>Species: </strong>'+custTreeDoc.tree.species+'</p>'+
+        '<p class="m-0 livemap-head"><strong>Size: </strong>'+custTreeDoc.tree.size+'ft</p>'+
+        '<p class="m-0 livemap-head"><strong>Label Colour: </strong>'+custTreeDoc.tree.label_colour+'</p>';
 
+        if(custDoc.delivery == 1) {
+             contentString += '<p class="m-1 livemap-head"><strong>Address line 1: </strong>' + custDoc.addr1;
 
-        //Now we've added the marker, we need to get the customer's details and add it to the marker
-        var tempTree = trees[i]
-        var customerID = tempTree.customer_id;
-        var customerDoc = customer_db.get(customerID);
+             if(custDoc.addr2 != "" || custDoc.addr2 != null) {
+                 contentString +='<strong>Address line 2: </strong>' + custDoc.addr2;
+             }
+             contentString += '<strong>Town: </strong>' + custDoc.town +
+             '<strong>Postcode: </strong>' + custDoc.postcode +
+             '</p>'
+         }
+         else {
+             contentString += '<p class="m-1"><strong>Customer has opted to COLLECT</strong></p>';
+         }
 
-        //Loop through customer, get content to put in infopanel
-        customerDoc.then(function(customer){
-
-            //Store customer in an array
-            customers.push(customer);
-
-            //Store customers in localstorage so we don't have to reaccess database
-            localStorage.setItem("customers", JSON.stringify(customers));
-
-
-            //Try catch, in case DB fetch fails.
-            try {
-                //Content for infowindow
-                contentString = '<div id="content-user-'+ customerID +'">'+
-                '<div id="siteNotice">'+
-                '</div>'+
-                '<h5 id="firstHeading" class="firstHeading">'+customer.first_name +" " + customer.last_name+'</h5>'+
-                '<div id="bodyContent">'+
-                '<p class="livemap-head"><strong>'+customer.email+'</strong></p>'+
-                '<p class="m-0 livemap-head"><strong>Customer Collection Date:</strong> '+tempTree.shipping_date+'</p>'+
-                '<p class="m-0 livemap-head"><strong>Tree Cutting Date: </strong>' +tempTree.cutting_info.cut_date+'</p>'+
-                '<p class="m-0 livemap-head"><strong>Tree ID: </strong>'+tempTree.tree.id+'</p>'+
-                '<p class="m-0 livemap-head"><strong>Species: </strong>'+tempTree.tree.species+'</p>'+
-                '<p class="m-0 livemap-head"><strong>Size: </strong>'+tempTree.tree.size+'ft</p>'+
-                '<p class="m-0 livemap-head"><strong>Label Colour: </strong>'+tempTree.tree.label_colour+'</p>';
-
-                if(customer.delivery == 1) {
-                     contentString += '<p class="m-1 livemap-head"><strong>Address line 1: </strong>' + customer.addr1;
-
-                     if(customer.addr2 != "" || customer.addr2 != null) {
-                         contentString +='<strong>Address line 2: </strong>' + customer.addr2;
-                     }
-                     contentString += '<strong>Town: </strong>' + customer.town +
-                     '<strong>Postcode: </strong>' + customer.postcode +
-                     '</p>'
-                 }
-                 else {
-                     contentString += '<p class="m-1"><strong>Customer has opted to COLLECT</strong></p>';
-                 }
-
-                 contentString += '<a id="userid-'+customerID+'" class="btn btn-primary" onclick="ReplaceInfoWindow(this)">Edit</a>' +
-                 '</div>'+
-                 '</div>';
-            }
-            catch(ex) {
-                console.log("And error occured. Error was " + ex);
-                console.log("Debugging:");
-                console.log(customer);
-            }
-
-            //Create infowindow using content we just generated
-            var infowindow = new google.maps.InfoWindow({
-                content: contentString,
-                maxWidth:250
-            }); //End Infowindow
-
-            //Add an event listener for the marker to open the infowindow
-            tempMarker.addListener('click', function(){
-                infowindow.open(map, tempMarker);
-            }); //End Event Listener
-
-        }); //End .then()
-
+         contentString += '<a id="userid-'+customerID+'" class="btn btn-primary" onclick="ReplaceInfoWindow(this)">Edit</a>' +
+         '</div>'+
+         '</div>';
     }
+    catch(ex) {
+        console.log("And error occured. Error was " + ex);
+        console.log("Debugging:");
+        console.log(custDoc);
+        console.log(treeDoc);
+    }
+
+    //Create infowindow using content we just generated
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString,
+        maxWidth:250
+    }); //End Infowindow
+
+    //Add an event listener for the marker to open the infowindow
+    tempMarker.addListener('click', function(){
+        infowindow.open(map, tempMarker);
+    }); //End Event Listener
 }
 
 function ReplaceInfoWindow(btn) {
